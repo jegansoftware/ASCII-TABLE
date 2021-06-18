c820ec114e321cda00c4993158f0874a92bcd23a
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-18 09:25:35 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+//File name Should be named as Ascii.java
+package ASCII;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Asciii n=new Asciii();
+		do {
+		n.get();
+		n.print();
+		
+		}while(1==1);
+	}
+
+}
